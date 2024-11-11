cc6b80e8fa546b6b2d1daedefd2a65b86785b728
shawshankrai <shashank.xx92@gmail.com>
2024-11-11 16:19:16 +0530

    done

---
+++ 344.reverse-string.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=344 lang=java
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+class Solution {
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+
+        while(i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+}
+// @lc code=end
+
