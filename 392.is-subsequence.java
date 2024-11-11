67d0de0652edf9cc65ffd07fc2cdfe1e0f7ffc30
shawshankrai <shashank.xx92@gmail.com>
2024-11-11 15:52:29 +0530

    done

--- 392.is-subsequence.java
+++ 392.is-subsequence.java
@@ -7,7 +7,22 @@
 // @lc code=start
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        
+        char[] firstArray = s.toCharArray();
+        char[] secondArray = t.toCharArray();
+
+        int i = 0;
+        int j = 0;
+
+        while(i < firstArray.length && j < secondArray.length) {
+            
+            if(firstArray[i] == secondArray[j]) {
+                i++;
+            } 
+            
+            j++;
+        }
+
+        return i == firstArray.length;
     }
 }
 // @lc code=end
