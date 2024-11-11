5b3a05c22c1c84882abbf9d272ef4187c36828b8
shawshankrai <shashank.xx92@gmail.com>
2024-11-11 15:19:02 +0530

    initial commit

---
+++ 392.is-subsequence.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=392 lang=java
+ *
+ * [392] Is Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+    }
+}
+// @lc code=end
+
